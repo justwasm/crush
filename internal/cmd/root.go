5794b078761b90aeeb9717a3c74fef08f688e5a5
Broken Pipe <navigaid@gmail.com>
2026-05-10 15:15:58 +0800

    boba: skip tea.WithEnvironment

--- internal/cmd/root.go
+++ internal/cmd/root.go
@@ -41,7 +41,6 @@ import (
 	ui "github.com/charmbracelet/crush/internal/ui/model"
 	"github.com/charmbracelet/crush/internal/version"
 	"github.com/charmbracelet/crush/internal/workspace"
-	uv "github.com/charmbracelet/ultraviolet"
 	"github.com/charmbracelet/x/ansi"
 	"github.com/charmbracelet/x/exp/charmtone"
 	xstrings "github.com/charmbracelet/x/exp/strings"
@@ -129,10 +128,8 @@ crush --continue
 		model := ui.New(com, sessionID, continueLast)
 
 		inputFilter := ui.NewFilter()
-		var env uv.Environ = os.Environ()
 		program := boba.NewProgram(
 			model,
-			tea.WithEnvironment(env),
 			tea.WithContext(cmd.Context()),
 			tea.WithFilter(inputFilter.Filter),
 		)
